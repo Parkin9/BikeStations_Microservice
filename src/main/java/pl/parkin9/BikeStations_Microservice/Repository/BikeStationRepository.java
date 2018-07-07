5354fe2d7aec3d14018cae70190c77798f881679
Piotr <piotrparkola@gmail.com>
2018-07-07 23:59:29 +0200

    Cleaning :)

--- src/main/java/pl/parkin9/BikeStations_Microservice/Repository/BikeStationRepository.java
+++ src/main/java/pl/parkin9/BikeStations_Microservice/Repository/BikeStationRepository.java
@@ -1,15 +1,13 @@
 package pl.parkin9.BikeStations_Microservice.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pl.parkin9.BikeStations_Microservice.Model.BikeStation;
-
-import java.util.Optional;
 
 @Repository
 public interface BikeStationRepository extends JpaRepository<BikeStation, Long> {
 
+    // Query to find BikeStation by Bike's serialNumber; unnecessary for now
 //    @Query("select bs from BikeStation bs join bst.bikeSet b where b.serialNumber = ?1")
 //    Optional<BikeStation> findBySerialNumber(String serialNumber);
 }
